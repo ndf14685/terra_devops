2583f7ac15914996963aad23c5730cab1871d5cf
Nestor Fleitas <david.fleitas@gmail.com>
2022-09-15 14:20:35 -0300

    Se edita script de instalacion

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -20,6 +20,23 @@ yum install docker -y
 #sudo service docker start
 sudo chkconfig docker on
 
+echo "Install Kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+chmod +x ./kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+#sudo mv ./kubectl /usr/local/bin/kubectl
+
+echo "Install AWS-IAM-Authenticator"
+curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/aws-iam-authenticator
+chmod +x ./aws-iam-authenticator
+sudo install -o root -g root -m 0755 aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
+mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
+#sudo mv ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
+
+
+
+
 echo "Install Jenkins"
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
