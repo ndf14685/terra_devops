1b6cd5a43b8981b47aec524baa1dac7bed2740b4
Nestor Fleitas <david.fleitas@gmail.com>
2022-09-14 23:34:18 -0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -57,6 +57,13 @@ resource "aws_security_group" "ec2-security-group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port       = 0
     to_port         = 0
