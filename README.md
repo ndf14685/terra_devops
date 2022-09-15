f0e7db3b3ddb35ffe9bd5a8e355aba34fd399f25
Nestor Fleitas <david.fleitas@gmail.com>
2022-09-14 21:46:45 -0300

    Se actualiza Readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # terra_devops
+
+Este repositorio tiene el codigo para crear a traves de terraform una vpc con 7 subnets, con security groups, un cluster de kubernetes, y una instaincia bastion con jenkins  dockerizado (con su propio sg). 
+
+Instala a demas repositorio ECR por si llego a necesitar desplegar
--- install_jenkins.sh
+++ install_jenkins.sh
@@ -4,17 +4,17 @@
 sudo yum -y update
 
 echo "Install Java JDK 8"
-yum remove -y java
-yum install -y java-1.8.0-openjdk
+sudo yum remove -y java
+sudo yum install -y java-1.8.0-openjdk
 
 echo "Install Maven"
 yum install -y maven 
 
 echo "Install git"
-yum install -y git
+sudo yum install -y git
 
 echo "Install Docker engine"
-yum update -y
+sudo yum update -y
 yum install docker -y
 #sudo usermod -a -G docker jenkins
 #sudo service docker start
@@ -23,7 +23,7 @@ sudo chkconfig docker on
 echo "Install Jenkins"
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-yum install -y jenkins
+sudo yum install -y jenkins
 sudo usermod -a -G docker jenkins
 sudo chkconfig jenkins on
 sudo service docker start
