4d68721879a6a0de0285e7b16acd4507f0890a33
Nestor Fleitas <david.fleitas@gmail.com>
2022-09-14 23:54:54 -0300

    Se edita security group

--- ec2.tf
+++ ec2.tf
@@ -52,14 +52,7 @@ resource "aws_security_group" "ec2-security-group" {
 
   ingress {
     from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 8000
-    to_port     = 8000
+    to_port     = 8081
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
